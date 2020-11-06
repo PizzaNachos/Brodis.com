e06cff78c03bfeae9f03f348e8060c9e4b4be362
brodis <brodis@andrews.edu>
2020-11-05 19:58:40 -0500

    working mandelbrot set

--- personal/alg/set.js
+++ personal/alg/set.js
@@ -1,79 +1,108 @@
-window.onload = function() {
-    tsParticles.loadJSON('particles-js', 'assets/particles.json').then(function(p) {
+window.onload = function () {
+  tsParticles
+    .loadJSON("particles-js", "assets/particles.json")
+    .then(function (p) {
       // p is the loaded container, for using it later
-      console.log('callback - particles.js config loaded');
+      console.log("callback - particles.js config loaded");
     });
-    tsParticles.load("tsparticles", {
-      particles: {
-        number: {
-          value: 120,
-          density: {
-            enable: true,
-            area: 800
-          }
-        },
-        color: {
-          value: ["rgb(0,0,0)"]
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#b6b2b2"
-          }
-        },
-        size: {
-          value: 2,
-          random: false,
-        },
-        lineLinked: {
+  tsParticles.load("tsparticles", {
+    particles: {
+      number: {
+        value: 120,
+        density: {
           enable: true,
-          distance: 130,
-          color: "rgb(0,0,0)",
-          opacity: 1,
-          width: 2,
+          area: 800,
         },
-        move: {
-          enable: true,
-          speed: .64,
-          direction: "none",
-          random: true,
-          straight: false,
-          outMode: "bounce",
-          bounce: false,
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200
-          }
-        }
       },
-    });
-  };
+      color: {
+        value: ["rgb(0,0,0)"],
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#b6b2b2",
+        },
+      },
+      size: {
+        value: 2,
+        random: false,
+      },
+      lineLinked: {
+        enable: true,
+        distance: 130,
+        color: "rgb(0,0,0)",
+        opacity: 1,
+        width: 2,
+      },
+      move: {
+        enable: true,
+        speed: 0.64,
+        direction: "none",
+        random: true,
+        straight: false,
+        outMode: "bounce",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+  });
+};
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "rgb(200,200,200)";
 
-function doStuff(){
-    for(let i = 0; i < canvas.height; i++){
-        for (let j = 0; j < canvas.width; j++) {
-            
-            
-        }
+let leftx = -1.5;
+let rightx = 0.5;
+let topy = 1;
+let bottomy = -1;
+
+doStuff();
+
+function doStuff() {
+  let px;
+  let py;
+  for (let i = 0; i < canvas.height; i++) {
+    py = translate(i, 0, canvas.height, bottomy, topy);
+    for (let j = 0; j < canvas.width; j++) {
+      px = translate(j, 0, canvas.width, leftx, rightx);
+      let test = testNum(px, py, 255);
+      ctx.fillStyle = "rgb(0,0," + test + ")";
+      ctx.fillRect(j, i, 1, 1);
     }
+  }
 }
-//translate(i, -1, 1, 0,1000)
-function translate(val,firstBound,secondBound,outputFirst,outputSecond){
-    let inputRange = secondBound - firstBound;
-    let outputRange = outputSecond - outputFirst;
-    let offset = firstBound - outputFirst;
+function testNum(real, imaginary, time) {
+  let previousReal = real;
+  let previousImaginary = imaginary;
+  let currentReal = 0;
+  let currentImaginary = 0;
+
+  for (let i = 0; i < time; i++) {
+    currentReal =
+      previousReal * previousReal -
+      previousImaginary * previousImaginary +
+      real;
+    currentImaginary = 2 * previousReal * previousImaginary + imaginary;
+
+    previousReal = currentReal;
+    previousImaginary = currentImaginary;
+
+    if (currentReal + currentImaginary > 4) {
+      return i;
+    }
+  }
+  return time;
 }
-let x = [];
-let y = [];
-for(let i = -100; i < 100; i++){
-    x.push(i);
-    y.push(translate(i,-100.0,100.0,-3.0,3.0));
+function translate(val, firstBound, secondBound, outputFirst, outputSecond) {
+  let normalVal = val - firstBound;
+  let inputRange = secondBound - firstBound;
+  let percentInput = normalVal / inputRange;
+  let outputRange = outputSecond - outputFirst;
+  return percentInput * outputRange + outputFirst;
 }
-console.log(x);
-console.log(y);
